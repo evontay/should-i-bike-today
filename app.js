0f38aa1ce9846d54776a7653b365cfd179312d83
Evon <fumblies@gmail.com>
2016-07-11 00:19:51 +0800

    fetched general data

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('app is running')
 console.clear()
 
 // *REFRESH
-// SHOW CURRENT DATE AND TIME
+// *SHOW CURRENT DATE AND TIME
 // GET MORNING (PERIOD 1) FORECAST FOR SOUTH AND EAST REGIONS
 // GET EVENING (PERIOD 3) FORECAST FOR SOUTH AND EAST REGIONS
 // IF PERIOD 1 AND PERIOD 3 SOUTH AND EAST IS 'Partly Cloudy' or 'Sunny'
@@ -33,24 +33,53 @@ $(document).ready(function () {
     }
   }
 
-  $.ajax(settings).done(function (response) {
-    console.log(response.items);
-  });
+  $.ajax(settings).done(function (data) {
+    console.log(data)
+    data.items.map(function (datum) {
+      $('#date').append(datum.timestamp)
+      console.log('timestamp:' + datum.timestamp)
+
+      $('#forecast').append('<p>' + datum.general.forecast + '</p>')
+      console.log('forecast:' + datum.general.forecast)
+
+      if (badWeather) {
+        $('body').css('background-color', '#555')
+        $('#verdict').html('<h1>Boo. Maybe not.</h1>')
+      }
+      else {
+        $('#verdict').html("<h1>It's a wonderful day. Grab your bike!</h1>")
+      }
+    })
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    console.log(errorThrown)
+  })
+
+  badWeather => {
+    $('#forecast:contains(Thunder), :contains(Rain), :contains(Shower), :contains(Drizzle)')
+  }
+  // $.ajax({
+  //   url: 'https://api.data.gov.sg/v1/environment/24-hour-weather-forecast'
+  // }).done (function (data) {
+  //   var now = data.map(function (response) {
+  //     return response
+  //   })
+  //   console.log('now: ' + now)
+  // })
 
   function reload () {
     $('#main').empty()
     $.get('https://api.data.gov.sg/v1/environment/24-hour-weather-forecast')
     .done(function (data) {
-      console.log(forEach)
+      // console.log('test: ' + response)
       data.forEach(function (datum) {
-        $('#main').append('<p>AS OF' + datum.objects.items + '</p>')
-        console.log('<p>AS OF' + datum.objects.items + '</p>')
+        $('#main').append('<p>' + datum + '</p>')
+        console.log('datum:' + datum)
       })
     }).fail(function (jqXHR, textStatus, errorThrown) {
       console.log(errorThrown)
     })
   }
-  //
+  // //
   // // Refresh the data
   // $('#refresh').click(function () {
   //   reload()
