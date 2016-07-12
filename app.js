d23b0460d6f89bafb0294bd9fdc011f667864057
Evon <fumblies@gmail.com>
2016-07-12 13:33:14 +0800

    updated badWeather logic and ok weather icon

--- app.js
+++ app.js
@@ -40,20 +40,21 @@ $(document).ready(function () {
       console.log('timestamp:' + datum.timestamp)
 
       $('#forecast').append('<p>' + datum.general.forecast + '</p>')
+      // $('#forecast').append('<p>Fine</p>')
       console.log('forecast:' + datum.general.forecast)
 
       $('#morning').append('<h3 class="underline">morning</h3> <p>' + datum.periods[1].regions.south + '</p>')
 
       $('#evening').append('<h3 class="underline">evening</h3> <p>' + datum.periods[2].regions.south + '</p>')
 
-      if (badWeather) {
-        $('body').css('background-color', '#555')
+      if (badWeather()) {
+        // $('body').css('background-color', '#555')
         $('#icon').html('<i class="wi wi-rain"></i>')
         $('#verdict').html('<h1>Boo. Maybe not.</h1>')
       }
       else {
         $('#verdict').html("<h1>Yes. Grab your bike!</h1>")
-        $('#icon').html('<i class="wi wi-sunny"></i>')
+        $('#icon').html('<i class="wi wi-day-sunny-overcast"></i>')
       }
     })
   }).fail(function (jqXHR, textStatus, errorThrown) {
@@ -61,7 +62,7 @@ $(document).ready(function () {
   })
 
   function badWeather(){
-    $('#forecast:contains(Thunder), :contains(Rain), :contains(Shower), :contains(Drizzle)')
+    $('#forecast:contains(Thunder), :contains(Rain), :contains(Shower), :contains(Drizzle)').css("background-color", "grey")
     return true;
   }
 
